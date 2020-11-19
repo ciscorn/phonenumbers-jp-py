1cd3b49f3b40cad3105eb1a4487fef41f75824ee
Taku Fukada <naninunenor@gmail.com>
2020-11-19 12:39:22 +0900

    Add basic tests

---
+++ tests/test_parse.py
@@ -0,0 +1,94 @@
+import jp_phone_numbers
+
+
+def test_parse_kotei():
+    info = jp_phone_numbers.parse("0344445555")
+    assert info.type == "固定"
+    assert info.parts == ["03", "4444", "5555"]
+    assert info.area_code == "03"
+    assert info.selected_carrier is None
+    assert info.message_area == "東京"
+
+    info = jp_phone_numbers.parse("0344445555")
+    assert info.type == "固定"
+    assert info.parts == ["03", "4444", "5555"]
+    assert info.area_code == "03"
+    assert info.message_area == "東京"
+
+    info = jp_phone_numbers.parse("0344445555")
+    assert info.type == "固定"
+    assert info.parts == ["03", "4444", "5555"]
+    assert info.area_code == "03"
+    assert info.message_area == "東京"
+
+    info = jp_phone_numbers.parse("0997223333")
+    assert info.type == "固定"
+    assert info.parts == ["0997", "22", "3333"]
+    assert info.area_code == "0997"
+    assert info.message_area == "種子島"
+
+    info = jp_phone_numbers.parse("0997998888")
+    assert info.type == "固定"
+    assert info.parts == ["0997", "99", "8888"]
+    assert info.area_code == "0997"
+    assert info.message_area == "徳之島"
+
+
+def test_parse_mobile():
+    info = jp_phone_numbers.parse("09011112222")
+    assert info.type == "携帯"
+    assert info.parts == ["090", "1111", "2222"]
+    assert info.area_code is None
+    assert info.message_area is None
+
+
+def test_parse_freedial():
+    info = jp_phone_numbers.parse("0120123456")
+    assert info.type == "フリーダイヤル"
+    assert info.parts == ["0120", "123", "456"]
+    assert info.callerid_delivery is None
+
+    info = jp_phone_numbers.parse("08001112222")
+    assert info.type == "フリーダイヤル"
+    assert info.parts == ["0800", "111", "2222"]
+    assert info.callerid_delivery is None
+
+
+def test_parse_m2m():
+    info = jp_phone_numbers.parse("02011122222")
+    assert info.type == "M2M"
+    assert info.parts == ["020", "111", "22222"]
+    assert info.callerid_delivery is None
+
+
+def test_parse_184_186():
+    info = jp_phone_numbers.parse("1840355556666")
+    assert info.area_code == "03"
+    assert info.callerid_delivery == "withhold"
+    assert info.parts == ["184", "03", "5555", "6666"]
+
+    info = jp_phone_numbers.parse("1860355556666")
+    assert info.area_code == "03"
+    assert info.callerid_delivery == "provide"
+    assert info.parts == ["186", "03", "5555", "6666"]
+
+
+def test_parse_carrier_selector():
+    info = jp_phone_numbers.parse("00330355556666")
+    assert info.area_code == "03"
+    assert info.callerid_delivery is None
+    assert info.parts == ["0033", "03", "5555", "6666"]
+
+
+def test_parse_184_and_carrier():
+    info = jp_phone_numbers.parse("18400330355556666")
+    assert info.area_code == "03"
+    assert info.callerid_delivery == "withhold"
+    assert info.parts == ["184", "0033", "03", "5555", "6666"]
+
+
+def test_parse_international():
+    info = jp_phone_numbers.parse("01081355554444")
+    assert info.type == "国際電話"
+    assert info.area_code is None
+    assert info.parts == ["010", "81355554444"]
